cf21085cd022cf66131363eebd9ec865f0ba04d7
Andrei Krylov <dronovoy@yandex.ru>
2023-02-10 23:28:12 +0300

    Homework was done

---
+++ Task64/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+
+Console.WriteLine("Введите число n");
+if (!int.TryParse(Console.ReadLine(), out int n) || n < 1)
+{
+    Console.WriteLine("Введено нецелое или отрицательное число или 0");
+    return;
+}
+
+void PrintNaturalsFromNto0(int n)
+{
+    if (n == 1) Console.Write($"{n} ");
+    else
+    {
+        Console.Write($"{n} ");
+        PrintNaturalsFromNto0(n - 1);
+    }
+}
+
+PrintNaturalsFromNto0(n);
---
+++ Task66/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите число M");
+if (!int.TryParse(Console.ReadLine(), out int M) || M < 1)
+{
+    Console.WriteLine("Введено нецелое или отрицательное число или 0");
+    return;
+}
+
+Console.WriteLine("Введите число N");
+if (!int.TryParse(Console.ReadLine(), out int N) || N < 1)
+{
+    Console.WriteLine("Введено нецелое или отрицательное число или 0");
+    return;
+}
+
+int SumsFromMtoN(int a, int b)
+{
+    if (a <= b)
+    {
+        if (b == a) return a;
+        else return b + SumsFromMtoN(a, b - 1);
+    }
+    else
+    {
+        if (a == b) return b;
+        else return a + SumsFromMtoN(a - 1, b);
+    }
+}
+
+Console.WriteLine($"Сумма чисел от {M} до {N} равна {SumsFromMtoN(M, N)}");
+
